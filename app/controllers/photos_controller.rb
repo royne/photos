c5508acc2fb752c40fb9f42622285621385c0cf3
royne <royne.chaves@gmail.com>
2018-04-23 18:29:45 -0500

    se agrega devise y modelo photos

---
+++ app/controllers/photos_controller.rb
@@ -0,0 +1,24 @@
+class PhotosController < ApplicationController
+  def index
+    @photos = Photo.all
+  end
+
+  def new
+    @photo = Photo.new
+  end
+
+  def create
+    @photo = Photo.create(photos_params)
+
+    if @photo.save
+      redirect_to root_path, notice: "La Evidencia fue creada con exito"
+    else
+      render :new
+    end
+  end
+
+  private
+    def photos_params
+      params.require(:photo).permit(:title)
+    end
+end
