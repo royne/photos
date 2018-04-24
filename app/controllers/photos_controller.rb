ce1906a623bf1c5774974c0c189cb99729a00144
royne <royne.chaves@gmail.com>
2018-04-23 19:17:20 -0500

    se agrega relacion de usuarios y photos y sea acomoda la grid

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -9,6 +9,7 @@ class PhotosController < ApplicationController
 
   def create
     @photo = Photo.create(photos_params)
+    @photo.user = current_user
 
     if @photo.save
       redirect_to root_path, notice: "La Evidencia fue creada con exito"
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180422015844) do
+ActiveRecord::Schema.define(version: 20180423234841) do
 
   create_table "photos", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_photos_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
