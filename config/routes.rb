0aa75ca1f69fd208886fc71da2bb7b65180de1dc
royne <royne.chaves@gmail.com>
2018-04-25 19:23:27 -0500

    se agrega modelo de cursos

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'courses/index'
+
   root 'photos#index'
 
   resources :photos
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180423234841) do
+ActiveRecord::Schema.define(version: 20180426001105) do
+
+  create_table "courses", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "photos", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.integer "course_id"
+    t.index ["course_id"], name: "index_photos_on_course_id"
     t.index ["user_id"], name: "index_photos_on_user_id"
   end
 
